f34094350abe888eaa031a3b0de38145e9d92746
Tomas Moreno Bauer <MorenoBauer10@gmail.com>
2026-02-13 04:47:51 -0300

    extras

--- app/__init__.py
+++ app/__init__.py
@@ -83,7 +83,6 @@ def create_app():
     app.register_blueprint(promo_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(auth_bp)
-    app.register_blueprint(AbonoHistorico)
 
     return app
 
